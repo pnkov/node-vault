d396ddf4d5bae863d12591c744cce25526d38b1a
pnkov <pn.kovaliov@gmail.com>
2020-08-12 04:15:14 +0300

    v0.9.21

--- types/index.d.ts
+++ types/index.d.ts
@@ -27,7 +27,7 @@ declare namespace NodeVault {
         };
     }
 
-    interface client extends commands {
+    export interface client extends commands {
         handleVaultResponse(res?: { statusCode: number, request: Option, body: any }): Promise<any>;
         apiVersion: string;
         endpoint: string;
